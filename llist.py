6430bbfbe0f86b084eaa433bfc5ae71864a3b6a5
njiche.dev <njicornelius@gmail.com>
2025-03-13 11:59:37 +0100

    List and list methods

--- llist.py
+++ llist.py
@@ -2,8 +2,72 @@
 #List are created using sqaure brackets
 ##propeties of list ; odered, changeable and they allow duplicate  values
 x = []
+y = 3
+z= 2.0
 
-myList = ["apple", "mangoes", "berries"]
-myList1 = [1,2,3,4,5,5, 6, 7,7,7]
-print(myList)
-print(myList1[0])
+list1 = ["apple", "mangoes", "berries"]
+list2 = [1,2,3,4,5,5, 6, 7,7,7]
+list3 = ['abc', 34, True, 40, "male"]
+#print(myList)
+#print(myList1[0])
+
+
+##list methods
+##Methods are inbuilt functions 
+# we can use to perform particular task
+print(len(list1))
+
+#type method
+print(type(z))
+
+#Accessing items
+print(list1[-2])
+
+
+#Range of Indexes
+print(list2[2:5])
+print(list3[:4])
+
+#Check if item exists
+if 40 in list3:
+    print("Yes, '40 is in list3")
+
+#Change item value
+list1[0] = 'Dragon fruit'
+#print(list1)
+
+##Change multiple item values
+list3[1:2] = ["Nji", "Diego"]
+#print(list3) 
+
+
+#insert items
+list1.insert(3, 'watermelon')
+#print(list1)
+
+
+##Exercise
+#Append 2 elemest to list 2 and 3 at index 1
+
+#Extend list
+list1.extend(list2)
+#print(list1)
+
+##Remove List elements
+list1.remove('berries')
+
+
+#remove specifird index
+list1.pop(5)
+print(list1)
+
+#Clear the list
+#clear() emmpties the list
+
+list2.clear()
+print(list2)
+help
+
+#loops list
+for p in list1:
+    print(p)
