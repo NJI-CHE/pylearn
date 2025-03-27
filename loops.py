1e6674eb18fe0531c0d3c0a8eb1f62d9b064efb9
njiche.dev <njicornelius@gmail.com>
2025-03-27 12:00:45 +0100

    Loops and conditionals

---
+++ loops.py
@@ -0,0 +1,9 @@
+#looping simply referes to repitition
+#Python has 2 primitive loops: The while and for loops
+
+##The while loop
+#The while executes a set of statements as a long as a condition is true
+i = 1
+while i < 6:
+    print(i)
+    i += 1
