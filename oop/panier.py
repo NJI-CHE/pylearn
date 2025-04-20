71f2cb7cae7a89426524b4de5dd8e6bc120372e1
njiche.dev <njicornelius@gmail.com>
2025-04-20 18:38:02 +0100

    Added cart class

---
+++ oop/panier.py
@@ -0,0 +1,53 @@
+#Define a class called Panier that represents a shopping cart
+class Panier:
+    #initialize the Panier with an empty
+    def __init__(self):
+        self.articles = []
+    
+
+    #Define a fxn to add articles to our parnier
+    def ajouter(self,nom_article, prix):
+        article = (nom_article, prix)
+        self.articles.append(article)
+
+    #Define the fxn to delete
+    def supprimer(self, nom_article):
+        for article in self.articles:
+            if article[0] == nom_article:
+                self.articles.remove(article)
+                break
+    
+    #Calculate and return total price
+    def Cal_total(self):
+        total = 0
+        for article in self.articles:
+            total += article[1]
+        return total
+    
+
+pan = Panier()
+
+#Ajouter les article
+pan.ajouter("Orange", 100)
+pan.ajouter("Spaghetti", 400 )
+pan.ajouter("Fromage", 50 )
+
+#Affiche les article dan le panier'
+print('Article dans le Panier')
+for article in pan.articles:
+    print(article[0], "-", article[1])
+
+
+#Calculate the total
+prix_total = pan.Cal_total()
+print("Le Prix total:", prix_total)
+
+#Supprimer un article
+pan.supprimer("Orange")
+print("\n Nouveua Panier:")
+for article in pan.articles:
+    print(article[0], "-", article[1])
+
+prix_total = pan.Cal_total()
+print("Total Quantity: ", prix_total)
+
