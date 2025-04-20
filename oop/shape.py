3759596305cb749abe0527ba0f5e31224269bb8a
njiche.dev <njicornelius@gmail.com>
2025-04-20 18:06:10 +0100

    added rectangle and triangle class

--- oop/shape.py
+++ oop/shape.py
@@ -73,8 +73,37 @@ circle = Circle(r)
 circle_area = circle.calculate_area()
 circle_perimiter = circle.calculate_perimeter()
 
+#print results for Curcle
 print("Radius of the Circle: ",r )
 print("Circle Area: ", circle_area)
 print("Circle Perimeter: ", circle_perimiter)
+
+
+#creating a rectnagle class
+l = 5
+w =7
+rectangle = Rectangle(l, w)
+rectangle_area = rectangle.calculate_area()
+rectangle_perimeter = rectangle.calculate_perimeter()
+
+
+#Print results for rectangle
+print("\nRectangle: Length =", l , "width =", w)
+print("\n Rectangle area:", rectangle_area)
+print("Rectangle Perimeter:", rectangle_perimeter)
         
-        
+
+#Create a triangle object
+base = 5
+height = 4
+s1 = 4
+s2 = 3
+s3 = 5
+
+#Print the results of the triangle
+print("\nTriangle: Base=", base, "Height=", height,  "side1 =", s1, " side2 =", s2, " side3 =", s3)
+triangle = Triangle(base, height, s1, s2, s3)
+triangle_area = triangle.calculate_area()
+triangle_perimeter = triangle.calculate_perimeter()
+print("Triangle Area:", triangle_area)
+print("Triangle Perimeter:", triangle_perimeter)
